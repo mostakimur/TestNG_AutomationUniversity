95f005e29644cf9508a2a9f16886a0261f4ec250
mostakimur <mostakim.rahman@gmail.com>
2023-11-05 17:01:32 +0600

    made some changes

--- src/testuniversity/Configuration_Annotations.java
+++ src/testuniversity/Configuration_Annotations.java
@@ -35,7 +35,7 @@ public class Configuration_Annotations {
 	@Test
 	public void searchCustomer() {
 		System.out.println("Search Customer!");
-		System.out.println(System.getProperty("user.dir"));
+		//System.out.println(System.getProperty("user.dir"));
 	}
 
 	@Test
